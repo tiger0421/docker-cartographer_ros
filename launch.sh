f57d330b6ed5459776b0108a72e3490e5f96a695
tiger0421 <taiger0421@yahoo.co.jp>
2020-04-24 02:14:49 +0900

    first commit

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+xhost +local:root
+
+docker run -it --name cartographer \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -e QT_X11_NO_MITSHM=1 \
+    --gpus all \
+    -w /root/ \
+    tiger0421/cartographer_ros:latest
+    /bin/bash
+
+xhost -local:root
